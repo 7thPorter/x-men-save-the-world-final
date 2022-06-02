de410229e671488f73d47d30aa0a7b3ca09de6be
Russell Porter <rusty@Russells-MacBook-Air.local>
2022-06-02 08:08:01 -0700

    Added Mystique transformations to villains.js

--- src/villains.js
+++ src/villains.js
@@ -1,4 +1,4 @@
-const villains = [
+export const villains = [
   {
     name: "Magneto",
     imageURL:
@@ -26,4 +26,18 @@ const villains = [
   },
 ];
 
-export default villains;
+export const mystique = [
+  "https://static.wikia.nocookie.net/all-worlds-alliance/images/a/ac/Uncanny_Cyclops_Portrait_Art.png",
+  "https://static.wikia.nocookie.net/characterprofile/images/5/5b/Wolverine_Portrait_Art.png",
+  "./psylocke.png",
+  "https://static.wikia.nocookie.net/vsbattles/images/3/38/Blue_Beast_MC_TR.png",
+  "https://static.miraheze.org/animatedmusclewomenwiki/f/fd/Rogue_%28Anna_Marie%29_in_Marvel_Avengers_Alliance_001.png",
+  "https://static.wikia.nocookie.net/vsbattles/images/8/8e/W35wrewrtgesrtregtqt64732547e623r45w76erg67rf32g6gfbuysdwfgui.png",
+  "https://freepngimg.com/thumb/xmen/21372-2-storm-x-men-clipart.png",
+  "https://static.wikia.nocookie.net/marveldatabase/images/d/d9/Piotr_Rasputin_%28Earth-12131%29_from_Marvel_Avengers_Alliance_001.png",
+  "https://static.wikia.nocookie.net/vsbattles/images/0/0d/Iceman_Comics.PNG",
+  "https://freepngimg.com/thumb/xmen/20689-8-jean-grey.png",
+  "https://static.wikia.nocookie.net/marveldatabase/images/e/e1/Warren_Worthington_III_%28Earth-TRN258%29_from_Marvel_Heroes_%28video_game%29.png",
+  "https://upload.wikimedia.org/wikipedia/en/8/80/Forge_%28Marvel_Comics_character%29.png",
+];
+
